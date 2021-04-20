e7aba02e223b9e8d68b759da3fdb3fa75dae46a7
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-04-20 23:55:08 +0300

    Add exception handling (easier for debug)

--- runtime/go_api.cpp
+++ runtime/go_api.cpp
@@ -1,4 +1,4 @@
-#include <runtime/runtime_plugin_interface.h>
+#include <runtime/runtime_plugin_api.h>
 #include <utils/scope_guard.hpp>
 #include <boost/filesystem.hpp>
 #include "functions_repository.h"
@@ -11,11 +11,18 @@ using namespace openffi::utils;
 	strcpy(*err, desc ); \
 	memset((*err+*err_len), 0, 1);
 
+#define catch_err(err, err_len, desc) \
+catch(std::exception& exc) \
+{\
+	handle_err(err, err_len, desc);\
+}
+
 #define handle_err_str(err, err_len, descstr) \
 	*err_len = descstr.length(); \
 	*err = (char*)malloc(*err_len + 1); \
 	descstr.copy(*err, *err_len, 0); \
 	memset((*err+*err_len), 0, 1);
+
 
 #define TRUE 1
 #define FALSE 0
@@ -32,8 +39,16 @@ int64_t load_function(const char* function_path, uint32_t function_path_len, cha
 	 * Load modules into modules repository - make sure every module is loaded once
 	 */
 	
-	// if fails throws an exception which is handled by xllr_api.cpp.
-	return functions_repository::get_instance().load_function(std::string(function_path, function_path_len));
+	try
+	{
+		return functions_repository::get_instance().load_function(std::string(function_path, function_path_len));
+	}
+	catch(std::exception& exc)
+	{
+		handle_err(err, err_len, exc.what());
+	}
+	
+	return -1;
 }
 //--------------------------------------------------------------------
 void free_function(int64_t function_id, char** /*err*/, uint32_t* /*err_len*/)
@@ -52,13 +67,17 @@ void call(
 	uint8_t* is_error
 )
 {
-	// get module
-	std::shared_ptr<foreign_function_entrypoint> func = functions_repository::get_instance().get_function(function_id);
-	
-	// call function
-	(*func)(in_params, in_params_len,
-			out_params, out_params_len,
-			out_ret, out_ret_len,
-			is_error);
+	try
+	{
+		// get module
+		std::shared_ptr<foreign_function_entrypoint> func = functions_repository::get_instance().get_function(function_id);
+		
+		// call function
+		(*func)(in_params, in_params_len,
+		        out_params, out_params_len,
+		        out_ret, out_ret_len,
+		        is_error);
+	}
+	catch_err((char**)out_ret, out_ret_len, exc.what());
 }
 //--------------------------------------------------------------------
